154c8204ce6227c19e74213b1ef21eeccd3baf58
wei-paul <paul.wei607@gmail.com>
2021-09-21 20:15:45 +0000

    trying out setTimeout

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+ let delay = 0;
+ for (const char of sentence) {
+    setTimeout(() => process.stdout.write(char), delay)
+    delay += 50;
+ }
+ 
